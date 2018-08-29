f14bbb58ec59472d3b5840d7764d75a012f3cacf
wz_shoegazer <1102472547@qq.com>
2018-08-29 16:20:52 +0800

    no.118

---
+++ N_118.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+	vector<vector<int>> generate(int numRows)
+	{
+		vector<vector<int>> res;
+		if(numRows <= 0) return res;
+		res.assign(numRows, vector<int>(1));
+		for (int i = 0; i < numRows; ++i)
+		{
+			res[i][0] = 1;
+			if(i == 0) continue;
+			for (int j = 1; j < i; ++j)
+			{
+				res[i].push_back(res[j - 1] + res[j]);
+			}
+			res[i].push_back(1);
+		}
+	}
+};
