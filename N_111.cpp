72157aaebd6de23094cd194de6cc55a7cc37837b
wz_shoegazer <1102472547@qq.com>
2018-08-29 09:35:31 +0800

    no.111

---
+++ N_111.cpp
@@ -0,0 +1,12 @@
+class Solution
+{
+public:
+	int minDepth(TreeNode *root)
+	{
+		if(!root) return 0;
+		if(!root->left && !root->right) return 1;
+		if(!root->left) return 1 + minDepth(root->right);
+		if(!root->right) return 1 + minDepth(root->left);
+		return 1 + min(minDepth(root->left), minDepth(root->right));
+	}
+};
