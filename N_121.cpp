462253e55fc37b2dca0bd92c4262cd57ebc52afc
wz_shoegazer <1102472547@qq.com>
2018-08-29 19:52:54 +0800

    no.121

---
+++ N_121.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+	int maxProfit(vector<int> &prices)
+	{
+		int res = 0, buy = INT_MAX;
+		for(int price : prices)
+		{
+			buy = min(buy, price);
+			res = max(res, price - buy);
+		}
+		return res;
+	}
+};
