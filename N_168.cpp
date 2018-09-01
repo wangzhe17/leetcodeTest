087736a194f80fed4a9f14c80051395db3459b06
wz_shoegazer <1102472547@qq.com>
2018-09-01 10:54:40 +0800

    no.168

---
+++ N_168.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+	string convertToTile(int n)
+	{
+        string res = "";
+        while (n) {
+            if (n % 26 == 0) {
+                res += 'Z';
+                n -= 26;
+            }
+            else {
+                res += n%26 - 1 + 'A';
+                n -= n%26;
+            }
+            n /= 26;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+
+	}
+}
