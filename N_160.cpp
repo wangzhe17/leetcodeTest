4ccb57eddd01402642963c8545425b3e59966c37
wz_shoegazer <1102472547@qq.com>
2018-09-01 08:44:29 +0800

    no.160

--- N_160.cpp
+++ N_160.cpp
@@ -39,4 +39,16 @@ public:
 		}
 		return cnt;
 	}
+
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+	{
+		if(!headA || !headB) return NULL;
+		ListNode *a = headA, *b = headB;
+		while(a != b)
+		{
+			a = a ? a->next : headB;
+			b = b ? b->next : headA;
+		}
+		return a;
+	}
 };
