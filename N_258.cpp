840fb70e575c28f3871e4ca8035c38cc12041411
wz_shoegazer <1102472547@qq.com>
2018-09-06 19:28:48 +0800

    no.258

---
+++ N_258.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num / 10 > 0)
+        {
+        	int sum = 0;
+        	while(num > 0)
+        	{
+        		sum += num % 10;
+        		num /= 10;
+        	}
+        	num = sum;
+        }
+
+        return sum;
+        // return (num - 1) % 9 + 1;
+    }
+};
