e1c147d1047bb6a44d9b87db7b77250937ca45fc
wz_shoegazer <1102472547@qq.com>
2018-09-12 22:50:55 +0800

    no.349

---
+++ N_349.cpp
@@ -0,0 +1,13 @@
+class Solution 
+{
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    {
+    	set<int> s(nums1.begin(), nums1.end()), res;
+    	for(auto a : nums2)
+    	{
+    		if(s.count(a)) res.insert(a);
+    	}
+    	return vector<int>(res.begin(), res.end());
+    }
+};
