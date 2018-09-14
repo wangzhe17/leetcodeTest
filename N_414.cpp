5ff0cf25120919e986d7e705347c6e5dd0947684
wz_shoegazer <1102472547@qq.com>
2018-09-14 21:25:41 +0800

    no.414

---
+++ N_414.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) 
+    {
+    	set<int> s;
+    	for(int num : nums)
+    	{
+    		s.insert(num);
+    		if(s.size() > 3)
+    		{
+    			s.erase(s.begin());
+    		}
+    	}
+    	return s.size() == 3 ? *s.begin() : s.*rbegin();
+    }
+};
