8217f6a7098088f25641b4d9682042487b55bbb6
wz_shoegazer <1102472547@qq.com>
2018-09-25 14:42:53 +0800

    no.507

---
+++ N_507.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) 
+    {
+        if(num == 1) return false;
+        int sum = 1;
+        for (int i = 2; i <= num; ++i)
+        {
+        	if(num % i == 0) sum += (i + num / i);
+        	if(i * i == num) sum -= i;
+        	if(sum > num) return false;
+        }
+        return sum == num;
+    }
+};
