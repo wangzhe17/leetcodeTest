14d907e91b55bbf1b14406fcb2d1db27c5278f29
wz_shoegazer <1102472547@qq.com>
2018-10-07 18:50:23 +0800

    no.771

---
+++ N_771.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numJewelsInStones(string J, string S) 
+    {
+    	int cnt = 0;
+        for (int i = 0; i < J.length(); ++i)
+        {
+        	for (int j = 0; j < S.length(); ++j)
+        	{
+        		if(J[i] == S[J]) ++cnt;
+        	}
+        }
+        return cnt;
+    }
+};
