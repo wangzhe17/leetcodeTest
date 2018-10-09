652d315995381ce5b6ad2171db9993e52648fec3
wz_shoegazer <1102472547@qq.com>
2018-10-09 22:18:47 +0800

    no.8

--- N_6.cpp
+++ N_6.cpp
@@ -1,20 +1,18 @@
-class Solution
-{
+class Solution {
 public:
-	string convert(string s, int nRows)
-	{
-		if (nRows <= 1) return s;
-		string res = "";
-		int size = 2 * nRows - 2;
-		for (int i = 0; i < nRows; ++i)
-		{
-			for (int j = i; j < s.size(); j += size)
-			{
-				res += s[j];
-				int tmp = j + size - 2 * i;
-				if(i != 0 && i != nRows - 1 && tmp < s.size()) res += s[tmp];
-			}
-		}
-		return res;
-	}
+    string convert(string s, int numRows) {
+        if(numRows <= 1) return s;
+        string res = "";
+        int size = 2 * numRows - 2;
+        for (int i = 0; i < numRows; ++i)
+        {
+        	for (int j = i; j < s.size(); j += size)
+        	{
+        		res += s[j];
+        		int tmp = j + size - 2 * i;
+        		if(i != 0 && i != numRows - 1 && tmp < s.size()) res += s[tmp];
+        	}
+        }
+        return res;
+    }
 };
